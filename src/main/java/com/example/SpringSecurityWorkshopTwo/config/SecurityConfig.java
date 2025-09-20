35ebb9194485ccf77b4a57ba86da8d25f4f01495
Wycliffe <ottawawycliffe@gmail.com>
2025-09-20 08:21:00 +0300

    Created a Bean of user details service and created hardcoded users and passed to the user details service

--- src/main/java/com/example/SpringSecurityWorkshopTwo/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityWorkshopTwo/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +24,22 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("Enrico")
+                .password("123456")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("Banks")
+                .password("123456")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 }
