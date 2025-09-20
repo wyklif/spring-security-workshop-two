5dc72d5b1015dd5a74128278bff25961b3046169
Wycliffe <ottawawycliffe@gmail.com>
2025-09-20 06:06:39 +0300

    initial

---
+++ src/main/java/com/example/SpringSecurityWorkshopTwo/SpringSecurityWorkshopTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityWorkshopTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWorkshopTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWorkshopTwoApplication.class, args);
+	}
+
+}
